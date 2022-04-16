043077b4ef0856c919702422f63fa02e3177b994
Sagar Nirwal <62455271+Sagar247776@users.noreply.github.com>
2022-04-16 11:33:57 +0530

    Front End Work

---
+++ components/Product.tsx
@@ -0,0 +1,160 @@
+import React from 'react'
+
+function Product() {
+  return (
+    <div>
+      <section className="body-font text-gray-600">
+        <div className="container mx-auto px-5 py-24">
+          <div className="-m-4 flex flex-wrap">
+            <div className="w-full p-4 md:w-1/2 lg:w-1/4">
+              <a className="relative block h-48 overflow-hidden rounded">
+                <img
+                  alt="ecommerce"
+                  className="block h-full w-full object-cover object-center"
+                  src="https://dummyimage.com/420x260"
+                />
+              </a>
+              <div className="mt-4">
+                <h3 className="title-font mb-1 text-xs tracking-widest text-gray-500">
+                  CATEGORY
+                </h3>
+                <h2 className="title-font text-lg font-medium text-gray-900">
+                  The Catalyzer
+                </h2>
+                <p className="mt-1">$16.00</p>
+              </div>
+            </div>
+            <div className="w-full p-4 md:w-1/2 lg:w-1/4">
+              <a className="relative block h-48 overflow-hidden rounded">
+                <img
+                  alt="ecommerce"
+                  className="block h-full w-full object-cover object-center"
+                  src="https://dummyimage.com/421x261"
+                />
+              </a>
+              <div className="mt-4">
+                <h3 className="title-font mb-1 text-xs tracking-widest text-gray-500">
+                  CATEGORY
+                </h3>
+                <h2 className="title-font text-lg font-medium text-gray-900">
+                  Shooting Stars
+                </h2>
+                <p className="mt-1">$21.15</p>
+              </div>
+            </div>
+            <div className="w-full p-4 md:w-1/2 lg:w-1/4">
+              <a className="relative block h-48 overflow-hidden rounded">
+                <img
+                  alt="ecommerce"
+                  className="block h-full w-full object-cover object-center"
+                  src="https://dummyimage.com/422x262"
+                />
+              </a>
+              <div className="mt-4">
+                <h3 className="title-font mb-1 text-xs tracking-widest text-gray-500">
+                  CATEGORY
+                </h3>
+                <h2 className="title-font text-lg font-medium text-gray-900">
+                  Neptune
+                </h2>
+                <p className="mt-1">$12.00</p>
+              </div>
+            </div>
+            <div className="w-full p-4 md:w-1/2 lg:w-1/4">
+              <a className="relative block h-48 overflow-hidden rounded">
+                <img
+                  alt="ecommerce"
+                  className="block h-full w-full object-cover object-center"
+                  src="https://dummyimage.com/423x263"
+                />
+              </a>
+              <div className="mt-4">
+                <h3 className="title-font mb-1 text-xs tracking-widest text-gray-500">
+                  CATEGORY
+                </h3>
+                <h2 className="title-font text-lg font-medium text-gray-900">
+                  The 400 Blows
+                </h2>
+                <p className="mt-1">$18.40</p>
+              </div>
+            </div>
+            <div className="w-full p-4 md:w-1/2 lg:w-1/4">
+              <a className="relative block h-48 overflow-hidden rounded">
+                <img
+                  alt="ecommerce"
+                  className="block h-full w-full object-cover object-center"
+                  src="https://dummyimage.com/424x264"
+                />
+              </a>
+              <div className="mt-4">
+                <h3 className="title-font mb-1 text-xs tracking-widest text-gray-500">
+                  CATEGORY
+                </h3>
+                <h2 className="title-font text-lg font-medium text-gray-900">
+                  The Catalyzer
+                </h2>
+                <p className="mt-1">$16.00</p>
+              </div>
+            </div>
+            <div className="w-full p-4 md:w-1/2 lg:w-1/4">
+              <a className="relative block h-48 overflow-hidden rounded">
+                <img
+                  alt="ecommerce"
+                  className="block h-full w-full object-cover object-center"
+                  src="https://dummyimage.com/425x265"
+                />
+              </a>
+              <div className="mt-4">
+                <h3 className="title-font mb-1 text-xs tracking-widest text-gray-500">
+                  CATEGORY
+                </h3>
+                <h2 className="title-font text-lg font-medium text-gray-900">
+                  Shooting Stars
+                </h2>
+                <p className="mt-1">$21.15</p>
+              </div>
+            </div>
+            <div className="w-full p-4 md:w-1/2 lg:w-1/4">
+              <a className="relative block h-48 overflow-hidden rounded">
+                <img
+                  alt="ecommerce"
+                  className="block h-full w-full object-cover object-center"
+                  src="https://dummyimage.com/427x267"
+                />
+              </a>
+              <div className="mt-4">
+                <h3 className="title-font mb-1 text-xs tracking-widest text-gray-500">
+                  CATEGORY
+                </h3>
+                <h2 className="title-font text-lg font-medium text-gray-900">
+                  Neptune
+                </h2>
+                <p className="mt-1">$12.00</p>
+              </div>
+            </div>
+            <div className="w-full p-4 md:w-1/2 lg:w-1/4">
+              <a className="relative block h-48 overflow-hidden rounded">
+                <img
+                  alt="ecommerce"
+                  className="block h-full w-full object-cover object-center"
+                  src="https://dummyimage.com/428x268"
+                />
+              </a>
+              <div className="mt-4">
+                <h3 className="title-font mb-1 text-xs tracking-widest text-gray-500">
+                  CATEGORY
+                </h3>
+                <h2 className="title-font text-lg font-medium text-gray-900">
+                  The 400 Blows
+                </h2>
+                <p className="mt-1">$18.40</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default Product
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,59 +1,20 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { FaFacebookF, FaGoogle, FaEnvelope } from 'react-icons/fa'
+import Footer from '../components/Footer'
+import Hero from '../components/Hero'
+import Navbar from '../components/Navbar'
+import Product from '../components/Product'
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div>
       <Head>
         <title>Eopen Store</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center bg-gray-100 px-20 text-center">
-        <div className="flex w-2/3 max-w-4xl rounded-2xl bg-white shadow-2xl">
-          <div className="w-3/5 p-5">
-            <div className="py-10">
-              <h2 className="text-3xl font-bold text-red-500">
-                Sign in to Account
-              </h2>
-              <div className="mb-2 inline-block w-10 border-2 border-red-500"></div>
-              <div className="my-2 flex justify-center">
-                <a
-                  href="/"
-                  className="mx-1 rounded-full border-2 border-gray-200 p-3"
-                >
-                  <FaFacebookF className="text-sm " />
-                </a>
-                <a
-                  href="/"
-                  className="mx-1 rounded-full border-2 border-gray-200 p-3"
-                >
-                  <FaGoogle className="text-sm " />
-                </a>
-              </div>
-              <p className="my-3 text-gray-400">or use your email account</p>
-              <div className="flex flex-col items-center">
-                <div className="w-64 bg-gray-100 p-2">
-                  <FaEnvelope />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="w-2/5 rounded-tr-2xl rounded-br-2xl bg-red-500 py-36 px-12 text-white">
-            <h2 className="mb-2 text-3xl font-bold">Login/SingUp!</h2>
-            <div className="mb-2 inline-block w-10 border-2 border-white"></div>
-            <p className="mb-10">
-              Fill up personal information and start journey with us.
-            </p>
-            <a
-              href="/"
-              className=" inline-block rounded-full border-2 border-white py-2 px-12 font-semibold hover:bg-white hover:text-red-500"
-            >
-              Sign Up
-            </a>
-          </div>
-        </div>
-      </main>
+      <Navbar />
+      <Hero />
+      <Product />
+      <Footer />
     </div>
   )
 }
